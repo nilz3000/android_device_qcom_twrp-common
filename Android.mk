409ebf060371550b32f4c9d4f73e594255692130
Captain Throwback <captainthrowback@hotmail.com>
2021-01-30 12:00:40 -0500

    tzdata: remove from repo in favor of upstream TWRP update
    tzdata will now be included by default
    
    Change-Id: Iaee71e2373164d2acb5d97b3099b4f69ca98f149

--- Android.mk
+++ Android.mk
@@ -15,26 +15,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# Dummy file to apply post-install patch for qcom_decrypt_fbe
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := tzdata_twrp
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)/sbin
-LOCAL_REQUIRED_MODULES := tzdata
-
-ifeq ($(BOARD_BUILD_SYSTEM_ROOT_IMAGE),true)
-    LOCAL_POST_INSTALL_CMD += \
-        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/system_root/system/usr/share/zoneinfo; \
-        cp -f $(TARGET_OUT)/usr/share/zoneinfo/tzdata $(TARGET_RECOVERY_ROOT_OUT)/system_root/system/usr/share/zoneinfo/;
-else
-    LOCAL_POST_INSTALL_CMD += \
-        mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/system/usr/share/zoneinfo; \
-        cp -f $(TARGET_OUT)/usr/share/zoneinfo/tzdata $(TARGET_RECOVERY_ROOT_OUT)/system/usr/share/zoneinfo/;
-endif
-include $(BUILD_PHONY_PACKAGE)
-
 ifeq ($(BOARD_USES_QCOM_FBE_DECRYPTION),true)
     BOARD_USES_QCOM_DECRYPTION := true
 
@@ -90,6 +70,7 @@ ifeq ($(BOARD_USES_QCOM_DECRYPTION),true)
 
     ifeq ($(BOARD_BUILD_SYSTEM_ROOT_IMAGE),true)
         LOCAL_POST_INSTALL_CMD += \
+            mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/system_root; \
             cp -Ra $(LOCAL_PATH)/crypto/system $(TARGET_RECOVERY_ROOT_OUT)/system_root/;
     endif
 
